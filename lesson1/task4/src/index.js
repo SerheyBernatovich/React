09a7cad57e940163a202425623350ad8beff15e9
SerheyBernatovich <sec1982@ukr.net>
2021-12-22 16:48:04 +0200

    add

---
+++ lesson1/task4/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <div className="greeting">
+    <h1 className="greeting__title">Hello, world!</h1>
+    <p className="greeting__text">I'm learning React</p>
+  </div>,
+  document.getElementById('root')
+);

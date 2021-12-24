84886dbdf51e97cc73648ca8f869b388af0d3746
SerheyBernatovich <sec1982@ukr.net>
2021-12-24 10:59:16 +0200

    add

---
+++ lesson3/task4/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Greeting from './Greeting';
+
+ReactDOM.render(
+  <Greeting
+    firstName="NaName"
+    lastName="LaLastName"
+    birthDate={new Date('2011-01-01T11:11:11:819Z')}
+  />,
+  document.getElementById('root')
+);

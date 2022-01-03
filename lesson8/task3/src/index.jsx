5b077ca4bafa1683f63f90df9801eadc53fc211f
SerheyBernatovich <sec1982@ukr.net>
2022-01-03 21:55:14 +0200

    add

---
+++ lesson8/task3/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Dimensions from './Dimensions.jsx';
+
+ReactDOM.render(<Dimensions />, document.getElementById('root'));

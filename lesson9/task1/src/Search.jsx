ef76d2a00a769b71e9747dd8d8c69858f24a0898
SerheyBernatovich <sec1982@ukr.net>
2022-01-04 17:13:43 +0200

    add

---
+++ lesson9/task1/src/Search.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  state = {
+    value: '',
+  };
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+  search = (e) => {
+    e.preventDefault();
+    alert(`Search text: ${this.state.value}`);
+  };
+  render() {
+    return (
+      <form className="search" onSubmit={this.search}>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+          className="search__input"
+        />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+export default Search;
---
+++ lesson9/task1/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Search from './Search.jsx';
+
+ReactDOM.render(<Search />, document.getElementById('root'));

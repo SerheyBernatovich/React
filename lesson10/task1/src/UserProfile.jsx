6deb4f81fe8ab9eaff0ca5fd9021d2a12545759d
SerheyBernatovich <sec1982@ukr.net>
2022-01-10 13:21:46 +0200

    add

---
+++ lesson10/task1/src/UserProfile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class UserProfile extends React.Component {
+  render() {
+    if (!this.props.userData) {
+      return null;
+    }
+    const { name, location, avatar_url } = this.props.userData;
+    return (
+      <div className="user">
+        <img src={avatar_url} alt="User Avatar" className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{name}</span>
+          <span className="user__location">{location}</span>
+        </div>
+      </div>
+    );
+  }
+}
+export default UserProfile;

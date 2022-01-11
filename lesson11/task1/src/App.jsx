59a95427963b6133fb49a8c2e02d46f858fadd5d
SerheyBernatovich <sec1982@ukr.net>
2022-01-11 12:05:08 +0200

    add

---
+++ lesson11/task1/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Numbers from './Numbers.jsx';
+import EvenNumbers from './EvenNumbers.jsx';
+import OddNumbers from './OddNumbers.jsx';
+
+class App extends React.Component {
+  state = { number: 0 };
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ number: this.state.number + 1 });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <div className="app">
+        <EvenNumbers title="Even numbers" number={this.state.number} />
+        <Numbers title="All numbers" number={this.state.number} />
+        <OddNumbers title="Odd numbers" number={this.state.number} />
+        <Numbers title="All numbers" number={17} />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ lesson11/task1/src/Numbers.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Number extends React.PureComponent {
+  //   shouldComponentUpdate(nextProps) {
+  //     if (this.props.number === nextProps.number && this.props.title === nextProps.title) {
+  //       return false;
+  //     }
+  //     return true;
+  //   }
+  render() {
+    return (
+      <div className="number">
+        <span className="number__title">{this.props.title}</span>
+        <span className="number__value">{this.props.number}</span>
+      </div>
+    );
+  }
+}
+export default Number;

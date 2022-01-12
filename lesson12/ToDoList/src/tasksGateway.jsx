59b3a1fc7e87a5320970be0ce013cb9ecf1f2403
SerheyBernatovich <sec1982@ukr.net>
2022-01-12 16:32:29 +0200

    add

---
+++ lesson12/ToDoList/src/tasksGateway.jsx
@@ -0,0 +1,43 @@
+const baseUrl = 'https://61b843c464e4a10017d18e3f.mockapi.io/api/v1/tasks';
+
+export const createTask = (taskData) =>
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error('Failed to create task');
+    }
+  });
+
+export const fetchTasksList = () =>
+  fetch(baseUrl).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  });
+
+export const updateTask = (taskId, taskData) =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskData),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error('Failed to updateTask task');
+    }
+  });
+
+export const deleteTask = (taskId) =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error('Failed to delete task');
+    }
+  });

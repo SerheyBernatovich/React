645783168d96966c11d3da49f509f6221817be8e
SerheyBernatovich <sec1982@ukr.net>
2022-01-14 16:55:52 +0200

    add

---
+++ lesson13/task2/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Products from './Products.jsx';
+import Home from './Home.jsx';
+
+const App = () => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+      </BrowserRouter>
+    </div>
+  );
+};
+export default App;
